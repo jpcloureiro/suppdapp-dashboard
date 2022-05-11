c6980af5b149bf1ba478ab35f6d304b44fc56532
Joao Loureiro <loureiro.jpc@gmail.com>
2022-05-11 23:20:46 +0100

    add modal component

---
+++ src/Modal.js
@@ -0,0 +1,51 @@
+import { Component, PropTypes } from 'react'
+
+class Modal extends Component {
+  constructor(props) {
+    super(props)
+    console.log(props)
+
+  }
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+
+  }
+
+  componentWillUnmount () {
+
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+Modal.propTypes = {
+  id: PropTypes.string.isRequired()
+}
+
+export default Modal
